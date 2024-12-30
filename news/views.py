474564d3bfd39dda8cd5262d66cef75c6ee29e5e
Adrian Nandu <adriannandu@gmail.com>
2024-12-31 01:21:43 +0800

    first commit

---
+++ news/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from .models import Topic
+
+def index(request):
+    """The home page for news"""
+    return render(request, 'news/index.html')
+
+def topics(request):
+    """Show all topics."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'news/topics.html', context)
+
+def topic(request, topic_id):
+    """Show a single topic and all its entries."""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'news/topic.html', context)
+
